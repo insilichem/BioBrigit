37f3f2fc865256622c98cf8ca9676546bacea597
Raúl Fernández Díaz <78709417+RaulFD-creator@users.noreply.github.com>
2025-06-12 10:12:42 +0100

    🚀 Update setup.py

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/insillichem/BioBrigit',
-    version='0.0.10',
+    version='1.0.0',
     zip_safe=False,
 )
